19df56b2cb8115990d51ab5691c8eac92bb081ca
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2023-01-20 00:04:16 +0100

    Added weekly updater

--- .config/zsh/pak_manager.sh
+++ .config/zsh/pak_manager.sh
@@ -2,6 +2,16 @@
 
 REPO_URL="https://github.com"
 
+# Time threshold
+TIME_THRESHOLD=604800    # 1 week in seconds
+#TIME_THRESHOLD=20 # 20 hours in seconds
+
+# Colors
+RED='\033[0;31m'
+NO_COLOR='\033[0m'
+GREEN='\033[0;32m'
+BRIGHT_CYAN='\033[0;96m'
+
 source_plugin() {
     if [ -f "$ZSH_PLUGIN_DIR/$1/$1.zsh" ]; then
         source "$ZSH_PLUGIN_DIR/$1/$1.zsh"
@@ -13,30 +23,42 @@ source_plugin() {
     elif [ -f "$ZSH_PLUGIN_DIR/$1/$1.zsh-theme" ]; then
         source "$ZSH_PLUGIN_DIR/$1/$1.zsh-theme"
     else
-        RED='\033[0;31m'
-        NO_COLOR='\033[0m'
         echo -e "${RED}Error adding plugin${NO_COLOR} $PLUGIN_NAME"
     fi
 }
 
+# Adds a plugin and sources it.
+# $1: user/plugin
+# $2: extra git params
 add_plugin() {
     PLUGIN_NAME=$(echo "$1" | cut -d "/" -f 2)
 
     #Se comprueba si existe el directorio, indicando que se ha descargado
     if [ ! -d "$ZSH_PLUGIN_DIR/$PLUGIN_NAME" ]; then
-
+        echo -e "\n$BRIGHT_CYAN####################################$NO_COLOR Installing $GREEN$PLUGIN_NAME$NO_COLOR $BRIGHT_CYAN####################################$NO_COLOR"
         # Si se pide algun comando extra a git, se pone como entrada a la funcion
         if [ "$#" -eq 2 ]; then
-        git clone "$2" "$REPO_URL/$1" "$ZSH_PLUGIN_DIR/$PLUGIN_NAME"
+            git clone "$2" "$REPO_URL/$1" "$ZSH_PLUGIN_DIR/$PLUGIN_NAME"
         else
-        git clone "$REPO_URL/$1" "$ZSH_PLUGIN_DIR/$PLUGIN_NAME"
+            git clone "$REPO_URL/$1" "$ZSH_PLUGIN_DIR/$PLUGIN_NAME"
         fi
 
+        #Se le añade una marca de tiempo para que cuando pase un tiempo determinado haga pull al plugin indicado
+        date +%s >"$ZSH_PLUGIN_DIR/.$PLUGIN_NAME"
+
+    # En caso de haberse pasado esa marca de tiempo, se le hace un pull al plugin para obtener los cambios
+    elif [ $(($(date +%s) - $(cat "$ZSH_PLUGIN_DIR/.$PLUGIN_NAME"))) -ge $TIME_THRESHOLD ]; then
+        echo "\n$BRIGHT_CYAN####################################$NO_COLOR Updating $GREEN$PLUGIN_NAME$NO_COLOR $BRIGHT_CYAN####################################$NO_COLOR"
+
+        cd "$ZSH_PLUGIN_DIR/$PLUGIN_NAME"
+        git pull
+        cd $HOME    # JUST IN CASE
+        date +%s > "$ZSH_PLUGIN_DIR/.$PLUGIN_NAME"
     fi
 
     source_plugin "$PLUGIN_NAME"
 }
 
-update_plugins() {
-    echo "mec"
+update_plugin() {
+
 }
--- .zshrc
+++ .zshrc
@@ -9,15 +9,9 @@ export ZSH_CONFIG_DIR="$HOME/.config/zsh"
 
 # Created by newuser for 5.9
 
+#Launch the completer
 autoload -U compinit
 compinit
-
-# source /usr/share/zinit/zinit.zsh
-# zinit light zsh-users/zsh-autosuggestions
-# zinit light zsh-users/zsh-history-substring-search
-# zinit light  zdharma-continuum/fast-syntax-highlighting 
-# zinit light zsh-users/zsh-completions
-#zinit ice depth=1; zinit light romkatv/powerlevel10k
 
 #source neccesary files
 source $ZSH_CONFIG_DIR/zsh-bindings.sh
