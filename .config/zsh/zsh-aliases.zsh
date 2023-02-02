5b72d18321c3ce6e5b94ef2dffbe624d41d12f4e
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2023-02-02 19:37:18 +0100

    Fixed git aliases

--- .config/zsh/zsh-aliases.zsh
+++ .config/zsh/zsh-aliases.zsh
@@ -15,8 +15,8 @@ alias yaycmo="yay -Qqd | yay -Rsu -"    #Clear more orphans
 
 #git aliases
 alias gitig="git clean -dfX"    #Clean ignored files
-alias gitprunemain="git remote prune origin && git branch --merged | grep -v main | xargs git branch -d"
-alias gitprunemaster="git remote prune origin && git branch --merged | grep -v master | xargs git branch -d"
+alias gitprunemain="git switch main && git remote prune origin && git branch --merged | grep -v main | xargs git branch -d"
+alias gitprunemaster="git switch master && git remote prune origin && git branch --merged | grep -v master | xargs git branch -d"
 
 
 alias re="source $HOME/.zshrc"
