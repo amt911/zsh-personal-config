fddc4eb68cd64ae2b0f31ed07791dd2d6eee189a
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2023-11-09 13:04:36 +0100

    Modified installation script

--- install.zsh
+++ install.zsh
@@ -11,6 +11,7 @@ readonly ZSH_REPO_PATH="$( cd -- "$( dirname -- "${(%):-%x}" )" &> /dev/null && 
 # We manually create the folder in order to avoid copying 
 # files to the repo and letting other plugins install on the same folder
 mkdir -p "$HOME/.config/zsh"
+mkdir -p "$HOME/.zsh-plugins"
 
 ln -sf "$ZSH_REPO_PATH/.zshrc" "$HOME/.zshrc"
 ln -sf "$ZSH_REPO_PATH/.p10k.zsh" "$HOME/.p10k.zsh"
