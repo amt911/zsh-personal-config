3ae24b593f03843a5ccf7e0f1e38eb64f9c323b9
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2024-05-05 22:50:39 +0200

    Added zsh option to avoid storing certain commands

--- .config/zsh/zsh-exports.zsh
+++ .config/zsh/zsh-exports.zsh
@@ -34,6 +34,7 @@ setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
 setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
 setopt HIST_BEEP                 # Beep when accessing non-existent history.
+setopt HIST_IGNORE_SPACE         # Do not store commands if they start with the space character
 
 export FZF_DIR_FILE_LOC="/usr/share/fzf/"       # Arch Linux location by default
 
